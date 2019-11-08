9f684a152c716db252cde6e02fac319d735ed42b
Sanjeev <34452724+Quanta-of-solitude@users.noreply.github.com>
2019-11-08 22:58:21 +0530

    no multiplexer

---
+++ noPlexer/SimpleAdafruit/SimpleAdafruit.ino
@@ -0,0 +1,112 @@
+/*
+ * Based on MQTT Adafruit Example, changes made accordingly
+ * Under FeedName avoid using <>
+ * 
+ * For IoT Development Board, 1.0 @Technomate Edubotics Pvt Ltd.
+ * 
+ */
+#include<ESP8266WiFi.h>
+#include "Adafruit_MQTT.h"
+#include"Adafruit_MQTT_Client.h"
+
+char ssid[] = " "; //Your Network SSID Here
+char password[] = " "; //Your Network password, here
+
+
+//Adafruit io setup...
+#define AIO_SERVER "io.adafruit.com"
+#define AIO_SERVERPORT 1883
+#define AIO_USERNAME " " //Adafruit Username
+#define AIO_KEY " " //Adafruit Secret Key
+
+#define ANALOG_INPUT A0
+
+float get_temp();
+
+WiFiClient client;
+
+
+Adafruit_MQTT_Client mqtt(&client,AIO_SERVER,AIO_SERVERPORT,AIO_USERNAME,AIO_KEY);
+/***********feeds************/
+Adafruit_MQTT_Publish TemperatureBoard = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME"/feeds/<FeedNameofTemperatureDisplay>");
+Adafruit_MQTT_Publish SoilMoisture = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME"/feeds/<FeedNameofSoilMoistureDisplay>");
+/***********feeds************/
+
+void MQTT_connect();
+
+
+void setup(){
+  Serial.begin(115200);
+  Serial.println();
+  Serial.println("connecting to.......");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid,password);
+  
+  while(WiFi.status()!= WL_CONNECTED)
+  {
+    delay(500);
+    Serial.println(".");
+  }
+  Serial.println();
+  Serial.println("Wifi connected...");
+  Serial.println("IP = ");
+  Serial.println(WiFi.localIP());
+  
+}
+
+
+ void loop(){
+
+  MQTT_connect();
+  
+  Serial.println("\nSending the Temperature");
+   if (! TemperatureBoard.publish(get_temp())){
+    Serial.println("Failed!");
+    }
+  else{
+    Serial.println("Value Sent!");  
+  }
+  delay(2000);
+}
+
+/***********Creating Connection************/
+void MQTT_connect(){
+  int8_t ret;
+  //stop if already connected
+  if(mqtt.connected())
+  {
+    return;
+  }
+  Serial.print("connecting to MQTT.......");
+  uint8_t retries=1;
+  while((ret=mqtt.connect())!=0)
+  {
+    Serial.println(mqtt.connectErrorString(ret));
+    Serial.println("Retrying MQTT connection in 5 seconds.....");
+    mqtt.disconnect();
+    delay(5000);
+    retries--;
+    if(retries==0)
+    {
+      while(1);
+      
+    }
+  }
+  Serial.println("MQTT CONNECTED.......//");
+}
+
+
+/***********Function For reading AnalogValue from board A0 pin************/
+float get_temp(){
+  
+  float temp_val;
+  int analogValue = analogRead(ANALOG_INPUT);
+  temp_val = (analogValue * 3.30);
+  delay(10); 
+  temp_val = (analogValue * 3.30);
+  delay(10);
+  temp_val = (temp_val/10); 
+  return temp_val;
+  
+}
